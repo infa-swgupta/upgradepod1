88912452f5337567dcdec043fbee9f3939e9ea9e
infa-swgupta <54552313+infa-swgupta@users.noreply.github.com>
2023-03-24 09:49:51 -0700

    Update TestCode1.java

---
+++ HackCode/TestCode1.java
@@ -0,0 +1,5 @@
+public class ValueOutOfRangeErrorExample {     
+public static void main(String[] args) {         
+int arr[] = new int[5];         
+System.out.println("5th element in array: " + arr[5]);     
+} }
